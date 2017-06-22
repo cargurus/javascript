52e8d9f34c26273e9e09f9ee32b6a2b2af176afb
jmong <jmong@cargurus.com>
2017-06-22 13:32:31 -0400

    Making it the CarGurus Style Guide

--- packages/eslint-config-cargurus/index.js
+++ packages/eslint-config-cargurus/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: [
-    'eslint-config-airbnb-base',
-    'eslint-config-airbnb-base/rules/strict',
+    'eslint-config-cargurus-base',
+    'eslint-config-cargurus-base/rules/strict',
     './rules/react',
     './rules/react-a11y',
   ].map(require.resolve),
