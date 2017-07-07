7953cad2061d6add09e94edc2d48921c90597d91
Jason Mong <jfm710@gmail.com>
2017-07-07 14:13:07 -0400

    remove js extension override

--- packages/eslint-config-cargurus-base/rules/imports.js
+++ packages/eslint-config-cargurus-base/rules/imports.js
@@ -134,10 +134,7 @@ module.exports = {
 
         // Ensure consistent use of file extension within the import path
         // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
-        'import/extensions': ['error', 'always', {
-            js: 'never',
-            jsx: 'never',
-        }],
+        'import/extensions': ['error', 'always'],
 
         // Enforce a convention in module import order
         // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
